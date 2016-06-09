1b579f28dd46154c6f00312b5a0ce5b60e3184a9
jmllegrand <jml.legrand@gmail.com>
2016-06-09 23:40:21 +0200

    feat(all): Make the U Testing works with stateless functional component

--- src/component/wine.spec.js
+++ src/component/wine.spec.js
@@ -5,17 +5,20 @@
 import {expect} from 'chai';
 import ReactTestUtils from 'react-addons-test-utils';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Wine from './wine';
 
 import {bootstrapEnv} from '../utils/bootstrap';
 
 describe('Wine', () => {
-  it('should display the correct name of the wine', () => {
+
+  before(() => {
     bootstrapEnv();
-    const wine = ReactTestUtils.renderIntoDocument(<Wine name='Chateau M' />);
-    //console.log('wine', wine);
-    const div = ReactTestUtils.findRenderedDOMComponentWithTag(wine, 'div');
-    console.log('div', div);
-    expect(div.textContent).to.be.equal('Chateau M');
+  });
+
+  it('should display the correct name of the wine', () => {
+    const wine = ReactTestUtils.renderIntoDocument(<div> <Wine name='Chateau M' /> </div>);
+    console.log('***** JM - wine component ***** \n ', wine);
+    expect(ReactDOM.findDOMNode(wine).children[0].textContent).to.be.equal('Chateau M');
   })
 });
--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -8,12 +8,10 @@ export function bootstrapEnv(body = '') {
   console.log('bootstrapEnv() start');
 
   const doc = jsdom.jsdom(`<!doctype html><html><body>${body}</body></html>`);
-  console.log('doc1', JSON.stringify(doc, null, 2));
 
   const win = doc.defaultView;
 
   function propagateToGlobal(window) {
-    console.log('propagateToGlobal() start');
 
     for (const key in window) {
       if (!window.hasOwnProperty(key)) continue;
@@ -23,8 +21,6 @@ export function bootstrapEnv(body = '') {
   }
 
   global.document = doc;
-  console.log('doc2', JSON.stringify(doc, null, 2));
-
   global.window = win;
   propagateToGlobal(win);
   console.log('\nENV setup is done !!!');
