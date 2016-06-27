eed27e327ca84916f23e1bf1737a87ae2b7302e4
jmllegrand <jml.legrand@gmail.com>
2016-06-27 06:58:00 +0200

    feat(all): routing (homemade) - WIP

--- src/app.js
+++ src/app.js
@@ -7,9 +7,47 @@ import ReactDOM from 'react-dom';
 import WineApp from './component/WineApp';
 import datas from './datas';
 
+import Home from './page/Home';
+
+const App = React.createClass({
+
+  getInitialState() {
+    debugger
+    console.log(window.location.hash.substr(1));
+    return {
+      route: window.location.hash.substr(1)
+    }
+  },
+
+  render() {
+    let Child;
+    switch (this.state.route) {
+      case '/about':
+        Child = About;
+        break;
+      case '/inbox':
+        Child = Inbox;
+        break;
+      default:
+        Child = Home;
+    }
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><a href="#/about">About</a></li>
+          <li><a href="#/inbox">Inbox</a></li>
+        </ul>
+        <Child />
+      </div>
+    )
+  }
+
+});
+
 
 ReactDOM.render(
-  <WineApp wines={datas}/>,
+  <App />,
   document.getElementById('main')
 );
 
---
+++ src/page/Home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jmlegrand on 27/06/16.
+ */
+
+
+import React from 'react';
+
+
+const Home = () =>
+  (<div>
+    <h1>THIS IS HOME</h1>
+  </div>);
+
+export default Home;
